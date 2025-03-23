c7ddecace5a39047755d8ab66418e5d02b25082e
Lauren Weiss <laurenweiss@Laurens-MacBook-Air.local>
2025-03-22 19:51:00 -0700

    Add homepage route to fix root URL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CheckIn from "./CheckIn";
-import BarView from "./BarView";
+// import Feed or other pages as needed
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<div style={{ padding: 40 }}><h1>Welcome to Seek</h1><p><a href="/checkin">Go to Check-In →</a></p></div>} />
         <Route path="/checkin" element={<CheckIn />} />
-        <Route path="/bar/:barName" element={<BarView />} />
+        {/* add more routes here like <Route path="/bar/:barName" element={<BarFeed />} /> */}
       </Routes>
     </Router>
   );
